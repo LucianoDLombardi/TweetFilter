27aac5262614d1a197ea11b8f7cfcf141b289d3b
Luciano Lombardi <ccianocian@gmail.com>
2018-10-17 07:20:37 -0700

    Major refactor of the code code. Added user input for start and end dates along with error checking.

--- TweetFilter/Program.cs
+++ TweetFilter/Program.cs
@@ -7,138 +7,214 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace TestConnect
+namespace TweetFilter
 {
+    //Tweet Model
     public class Tweet
     {
         public string id { get; set; }
         public DateTime stamp { get; set; }
         public string text { get; set; }
+    }
 
-        public class TweetComparer : IEqualityComparer<Tweet>
+    //Class to check for duplicate tweets
+    public class TweetComparer : IEqualityComparer<Tweet>
+    {
+        public bool Equals(Tweet x, Tweet y)
         {
-            public bool Equals(Tweet x, Tweet y)
-            {
-                if (x == null || y == null)
-                    return false;
-                else if (x.text == y.text)
-                    return true;
-                else
-                    return false;
-            }
+            if (x == null || y == null)
+                return false;
+            else if (x.text == y.text)
+                return true;
+            else
+                return false;
+        }
 
-            public int GetHashCode(Tweet obj)
-            {
-                return obj.text.GetHashCode();
-            }
+        public int GetHashCode(Tweet obj)
+        {
+            return obj.text.GetHashCode();
         }
     }
 
     class Program
     {
+        static string baseURI = "https://badapi.Iqvia.io/";
+
         static void Main(string[] args)
         {
-            Task t = new Task(DownloadPageAsync);
-            t.Start();
+            //Set window to be a bit wider than default
+            Console.WindowWidth = (int)(0.8 * Console.LargestWindowWidth);
+
+            //Increase the buffer height to attempt to show all tweets
+            Console.BufferHeight = 20000;
+
+            //Get dates from user and check if they are valid
+            if (!GetDatesFromUser(out string startDate, out string endDate))
+                return;
+
             Console.WriteLine("Downloading tweets...");
+
+            //Retrieve and output tweets
+            DownloadTweets(startDate, endDate);
+
             Console.ReadLine();
         }
 
-
-        static async void DownloadPageAsync()
+        static async void DownloadTweets(string startDate, string endDate)
         {
-            //Stopwatch sw = new Stopwatch();
-            //sw.Start();
-
+            //List of tweets with no duplicates
             List<Tweet> filteredtweets = new List<Tweet>();
-            List<Tweet> tweets = new List<Tweet>();
 
-            int nSumUnique = 0;
-            // ... Target page.
-            string page = "https://badapi.Iqvia.io/";
+            // Target.
+            string strGetRequest = "api/v1/Tweets?startDate=" + startDate + "&endDate=" + endDate;
 
-            string strNewStartDate = "2016-01-01T00:00:00";
-            string strEndDate = "2018-01-01T00:00:00";
-            string strGetRequest = "api/v1/Tweets?startDate=" + strNewStartDate + "&endDate=" + strEndDate;
-
-            // ... Use HttpClient.
-            using (HttpClient client = new HttpClient())
+            // Use HttpClient.
+            HttpClient client = new HttpClient()
             {
-                client.BaseAddress = new Uri(page);
-                client.DefaultRequestHeaders.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                try
+                BaseAddress = new Uri(baseURI)
+            };
+
+            try
+            {
+                //Make the get request
+                HttpResponseMessage response = await client.GetAsync(strGetRequest);
+
+                var responsePhrase = response.ReasonPhrase;
+                if (!responsePhrase.Equals("OK"))
                 {
-                    HttpResponseMessage response = await client.GetAsync(strGetRequest);
-                    HttpContent content = response.Content;
+                    Console.WriteLine("Get request failed due to {0}", responsePhrase);
+                    return;
+                }
+
+                HttpContent content = response.Content;
+
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                //Initialize deserialization with datetime using UTC
+                Newtonsoft.Json.JsonSerializerSettings js = new Newtonsoft.Json.JsonSerializerSettings()
+                {
+                    DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc
+                };
+
+                //Get the list of responses
+                var tweetResult = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Tweet>>(result, js);
+
+                //Initialize comparer to check for duplcate twees
+                var tweetComp = new TweetComparer();
+                var tweetResultDistinct = tweetResult.Distinct<Tweet>(tweetComp).AsParallel().ToList<Tweet>();
+
+                //Populate filtered list with the first Get results
+                filteredtweets = tweetResultDistinct.ToList();
+
+                // Now loop until we have reached the end date. We break out when we have a retrieval count < 100
+                while (tweetResult.Count > 0)
+                {
+                    //Set new start date to the date of the last item in the previous set of tweets
+                    var newStartDate = tweetResultDistinct[tweetResultDistinct.Count - 1].stamp.ToString("yyyy-MM-ddTHH:mm:ssZ");
+                    strGetRequest = "api/v1/Tweets?startDate=" + newStartDate + "&endDate=" + endDate;
+
+                    response = await client.GetAsync(strGetRequest);
+                    content = response.Content;
 
                     // ... Read the string.
-                    string result = await content.ReadAsStringAsync();
+                    result = await content.ReadAsStringAsync();
 
-                    Newtonsoft.Json.JsonSerializerSettings js = new Newtonsoft.Json.JsonSerializerSettings()
+                    tweetResult = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Tweet>>(result, js);
+
+                    //Avoid duplicates in the get
+                    tweetResultDistinct = tweetResult.Distinct<Tweet>(tweetComp).AsParallel().ToList<Tweet>();
+
+                    //Add to the filtered tweet list
+                    foreach (var tweet in tweetResultDistinct)
                     {
-                        DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc
-                    };
-
-                    var tweetResult = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Tweet>>(result, js);
-
-                    var tweetResultDistinct = tweetResult.Distinct<Tweet>(new Tweet.TweetComparer()).AsParallel().ToList<Tweet>();
-
-                    filteredtweets = tweetResultDistinct.ToList();
-
-
-                    DateTime finalDT = new DateTime(2017, 1, 1, 0, 0, 0);
-                    while (tweetResultDistinct[tweetResultDistinct.Count - 1].stamp <= finalDT)
-                    {
-                        strNewStartDate = tweetResultDistinct[tweetResultDistinct.Count - 1].stamp.ToString("yyyy-MM-ddTHH:mm:ssZ");
-                        strGetRequest = "api/v1/Tweets?startDate=" + strNewStartDate + "&endDate=" + strEndDate;
-
-                        response = await client.GetAsync(strGetRequest);
-                        content = response.Content;
-
-                        // ... Read the string.
-                        string result2 = await content.ReadAsStringAsync();
-
-                        tweetResult = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Tweet>>(result2, js);
-
-                        //Avoid duplicates in the get
-                        tweetResultDistinct = tweetResult.Distinct<Tweet>(new Tweet.TweetComparer()).AsParallel().ToList<Tweet>();
-                        
-                        foreach (var tweet in tweetResultDistinct)
-                        {
-                            if (!filteredtweets.Contains(tweet, new Tweet.TweetComparer()))
-                                filteredtweets.Add(tweet);
-                        }
-
-                        if (tweetResult.Count < 100) //We have reached the final group of tweets within the date range
-                            break;
+                        //Avoid adding duplicates to the full filtered list
+                        if (!filteredtweets.Contains(tweet, tweetComp))
+                            filteredtweets.Add(tweet);
                     }
 
+                    //We have reached the final group of tweets within the date range
+                    if (tweetResult.Count < 100)
+                        break;
+                }
 
-                }
-                catch (Exception e)
-                {
-                    string error = e.ToString();
-                    Console.WriteLine(error);
-                }
+            }
+            catch (Exception e)
+            {
+                string error = e.ToString();
+                Console.WriteLine(error);
+                return;
             }
 
 
-            // sw.Stop();
-            // var time = sw.ElapsedMilliseconds;
-
-            // ... Display the result.
+            // ... Display the results
             foreach (var tweet in filteredtweets)
             {
-
                 Console.WriteLine("ID: {0}, {1}", tweet.id, tweet.stamp);
                 Console.WriteLine("Tweet: {0}\n", tweet.text);
             }
 
-            Console.WriteLine("Total number of distinct tweets in 2016 and 2017 is {0}", filteredtweets.Count);
+            Console.WriteLine("Total number of unique tweets between {0} and {1} is {2}", startDate, endDate, filteredtweets.Count);
 
+        }
 
+        static bool AreDatesValid(string start, string end)
+        {
+            var startDate = new DateTime();
+            if (!DateTime.TryParse(start, out startDate))
+            {
+                Console.WriteLine("Invalid start date");
+                return false;
+            }
 
+            var endDate = new DateTime();
+            if (!DateTime.TryParse(end, out endDate))
+            {
+                Console.WriteLine("Invalid end date");
+                return false;
+            }
+
+            if (DateTime.Compare(startDate, endDate) > 0)
+            {
+                Console.WriteLine("Start date cannot be after the end date.");
+                return false;
+            }
+
+            if (DateTime.Compare(startDate, endDate) == 0)
+            {
+                Console.WriteLine("Start date and end date cannot be the same value.");
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool GetDatesFromUser(out string startDate, out string endDate)
+        {
+            startDate = "";
+            endDate = "";
+
+            var DatesValid = false;
+
+            while (!DatesValid)
+            {
+                Console.WriteLine("Please enter the start date for tweet retrieval (ex. 2016-01-01T00:00:00, note: time is optional). Press q to quit");
+                startDate = Console.ReadLine();
+
+                if (startDate.ToLower() == "q")
+                    return false;
+
+                Console.WriteLine("Please enter the end date for tweet retrieval (ex. 2018-01-01T00:00:00, note: time is optional). Press q to quit");
+                endDate = Console.ReadLine();
+
+                if (endDate.ToLower() == "q")
+                    return false;
+
+                DatesValid = AreDatesValid(startDate, endDate);
+
+            }
+
+            return true;
         }
     }
 }
